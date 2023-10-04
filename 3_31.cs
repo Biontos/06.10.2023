efc82719d33e56085e203ee1390611e9232a8019
Biontos <116175438+Biontos@users.noreply.github.com>
2023-10-04 23:40:56 +0300

    Add files via upload

---
+++ 3_31.cs
@@ -0,0 +1,9 @@
+﻿// Задание 3 стр 31
+Console.Write("Введите количество часов : ");
+int n = int.Parse(Console.ReadLine());
+int ameba = 1;
+for (int hours = 3; hours <= n; hours += 3)
+{
+    ameba *= 2;
+}
+Console.WriteLine($"Через {n} часов будет {ameba} амеб");
