fcec5e57cc13a2dda955260bbb2541e4ea427050
Biontos <116175438+Biontos@users.noreply.github.com>
2023-10-04 23:51:32 +0300

    Add files via upload

---
+++ 6_38.cs
@@ -0,0 +1,8 @@
+﻿// задание 6 стр 38
+int[] sequence = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+int product = 1;
+foreach (int number in sequence)
+{
+    product *= number;
+}
+Console.WriteLine($"Произведение элементов последовательности: {product}");
