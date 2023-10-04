240f82474b4cd5d1333a04037ed5227d53ece7bb
Biontos <116175438+Biontos@users.noreply.github.com>
2023-10-04 23:49:43 +0300

    Add files via upload

---
+++ 1_38.cs
@@ -0,0 +1,8 @@
+﻿// задание 1 стр 38
+int[] sequence = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+int sum = 0;
+foreach (int number in sequence)
+{
+    sum += number;
+}
+Console.WriteLine($"Сумма элементов последовательности: {sum}");
