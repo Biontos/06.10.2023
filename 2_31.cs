e2d90317e006bb989952c9f7798aead7b14f79ab
Biontos <116175438+Biontos@users.noreply.github.com>
2023-10-04 23:44:36 +0300

    Add files via upload

---
+++ 2_31.cs
@@ -0,0 +1,20 @@
+﻿// задание 2 стр 31
+double distance = 10;
+double dailyIncrease = 0.10;
+int totalDays = 0;
+Console.Write("Введите количество дней : ");
+if (int.TryParse(Console.ReadLine(), out totalDays) && totalDays > 0)
+{
+    double totalDistance = 0;
+    for (int day = 1; day <= totalDays; day++)
+    {
+        totalDistance += distance;
+        distance += distance * dailyIncrease;
+    }
+
+    Console.WriteLine($"Спортсмен пробежит {totalDistance:F2} км за {totalDays} дней");
+}
+else
+{
+    Console.WriteLine("Число не подходит");
+}
