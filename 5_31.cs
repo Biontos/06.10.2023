edf15190b9e8ceb5933a2aef327fc2840e0bb764
Biontos <116175438+Biontos@users.noreply.github.com>
2023-10-04 23:41:31 +0300

    Add files via upload

---
+++ 5_31.cs
@@ -0,0 +1,11 @@
+﻿// задание 5 стр 31
+int legs = 64;
+for (int goose= 0; goose <= legs/ 2; goose++)
+{
+    int rab = (legs- (goose * 2))/ 4;
+    int remlegs = legs - (goose * 2) - (rab * 4);
+    if (remlegs == 0)
+    {
+        Console.WriteLine($"Гусей: {goose}, Кроликов: {rab}");
+    }
+}
